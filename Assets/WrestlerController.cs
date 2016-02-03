ee58fa37153f82da5105ff100bef1a3879089a75
wingcutangel <wingcutangel@gmail.com>
2016-02-03 18:50:12 +0100

    First upload
    Basics with a starting player controller and arena

---
+++ Assets/WrestlerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WrestlerController : MonoBehaviour {
+	float deltaX;
+	float deltaY;
+
+	Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>(); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		deltaX = Input.GetAxis("Horizontal");
+		deltaY = Input.GetAxis("Vertical");
+
+		rigidbody.AddForce(transform.forward * deltaY * 10f, ForceMode.Force);
+		rigidbody.AddTorque(transform.up * deltaX * 1f);
+	}
+}
